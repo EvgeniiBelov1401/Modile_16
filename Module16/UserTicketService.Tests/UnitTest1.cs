c0b3ebb93cf8a0ef6721c3c51b1fd14eb79e5622
Евгений <evgeniibelov1401@gmail.com>
2024-06-08 14:12:26 +0300

    XUnit

---
+++ Module16/UserTicketService.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UserTicketService.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(50==100);
+        }
+    }
+}
---
+++ UserTicketService/UserTicketService.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UserTicketService.Tests
+{
+     class CalculatorTests
+    {
+
+        public void MultiplicationMustReturnNotNullValue()
+        {
+            var calculator =new Calculator();
+        }
+    }
+}
